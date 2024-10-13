588a9388aacdd44f259e8926ebb4e20689daf221
Victor Luís <victorluiscaguiar@gmail.com>
2024-10-13 17:09:35 -0300

    feat: add store logic and create models for posts and users

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/victorluisca/gopher-social/internal/env"
+	"github.com/victorluisca/gopher-social/internal/store"
 )
 
 func main() {
@@ -11,8 +12,11 @@ func main() {
 		addr: env.GetString("ADDR", ":8080"),
 	}
 
+	store := store.NewStorage(nil)
+
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
 
 	mux := app.mount()
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/victorluisca/gopher-social
 
 go 1.23.2
 
-require github.com/go-chi/chi/v5 v5.1.0
+require (
+	github.com/go-chi/chi/v5 v5.1.0
+	github.com/lib/pq v1.10.9
+)
